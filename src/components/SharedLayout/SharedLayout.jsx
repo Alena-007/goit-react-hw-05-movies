828376f49a7b5964910717151136bf4058735eda
Alena-007 <alena.tv@ukr.net>
2022-10-24 19:19:57 +0300

    add NavLink && styled-components && styled-system

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,26 @@
+import { Outlet, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const SharedLayout = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <StyledLink to="/" end>
+            Home
+          </StyledLink>
+          <StyledLink to="/movies">Movies</StyledLink>
+        </nav>
+      </header>
+      <Outlet />
+    </div>
+  );
+};
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
